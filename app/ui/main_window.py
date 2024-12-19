3d070b02458661dd1d87d0ed48657e0a6cc90337
Kia <kiasambrook@btinternet.com>
2024-12-19 12:44:06 +0000

    feat: populate dropdown list with real stop points

--- app/ui/main_window.py
+++ app/ui/main_window.py
@@ -1,28 +1,33 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
 from ui.dropdown import Dropdown
+from api.tfl_api import TfLAPI
+from config import TFL_API_KEY
 
 
 class MainWindow(App):
     def build(self):
         # Load the KV file
         Builder.load_file("ui/main_window.kv")
+        main_layout = BoxLayout(orientation="vertical")
+
+        api = TfLAPI(TFL_API_KEY)
+        stop_points = api.fetch_all_stop_points()
 
         # Create the DropDown widget
         dropdown_widget = Dropdown()
+        dropdown_options = {}
 
-        # Define options as text-value pairs
-        dropdown_options = {
-            "Oxford": "1",
-            "Cambridge": "2",
-            "London": "3",
-            "Manchester": "4",
-        }
+        for stop_point in stop_points.get("stopPoints", []):
+            dropdown_options[stop_point.get("commonName")] = stop_point.get("id")
 
-        # Set the dropdown options dynamically
         dropdown_widget.dropdown_options = dropdown_options
 
-        return dropdown_widget
+        # Add the DropDown widget to the main layout
+        main_layout.add_widget(dropdown_widget)
+
+        return main_layout
 
 
 if __name__ == "__main__":
